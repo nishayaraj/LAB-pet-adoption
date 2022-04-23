ddccdb6b00d042f70eb1732da36e20da53e1fd6a
nishaya raj <nishayaraj@gmail.com>
2022-04-22 21:44:45 -0500

    updated images

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const pets = [
     specialSkill:
       "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
     type: "dino",
-    imageUrl: "http://www.jozilife.co.za/wp-content/uploads/The-Dino-Expo.jpg",
+    imageUrl: "https://nationalzoo.si.edu/sites/default/files/styles/480x240_scale_and_crop/public/attractions/dinobot-trex-03.jpg?itok=YRsHp3EX&timestamp=1557239634",
   },
   {
     id: 3,
@@ -69,7 +69,7 @@ const pets = [
     specialSkill: "Comfortable in the outdoors for up to eight hours.",
     type: "dino",
     imageUrl:
-      "http://www.theouthousers.com/images/jck//ThanosCopter/news/grumpasaur.jpg",
+      "https://images.unsplash.com/photo-1606856110002-d0991ce78250?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
   },
   {
     id: 9,
@@ -96,7 +96,7 @@ const pets = [
     specialSkill: "Can be around food without staring creepily at it.",
     type: "dino",
     imageUrl:
-      "http://www.cantref.com/userfiles/events/walking%20dino.jpg?w=600",
+      "https://do512family.com/wp-content/uploads/2020/07/IMG_3536-1.jpg",
   },
   {
     id: 12,
@@ -142,7 +142,7 @@ const pets = [
     specialSkill: "Drives at a safe rate of speed in snow or rain.",
     type: "dino",
     imageUrl:
-      "https://images.readwrite.com/wp-content/uploads/2018/03/t-rex-dino-quiz-e1490854556549.jpg",
+      "https://static.scientificamerican.com/sciam/cache/file/B4520B4E-BCC1-411C-BEC6541928BDB992_source.jpg?w=590&h=800&9DD618C2-44B0-4723-A4E1CF610359A966",
   },
   {
     id: 17,
@@ -151,7 +151,7 @@ const pets = [
     specialSkill:
       "Does not freak out if you haven’t seen his favorite movie (The Big Lebowski).",
     type: "cat",
-    imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg",
+    imageUrl: "https://images.immediate.co.uk/production/volatile/sites/23/2014/10/GettyImages-172050389-8ab8710.jpg?quality=45&resize=768,574",
   },
   {
     id: 18,
@@ -160,7 +160,7 @@ const pets = [
     specialSkill: "Proficient in air guitar",
     type: "dino",
     imageUrl:
-      "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg",
+      "https://deseret.brightspotcdn.com/dims4/default/9beb006/2147483647/strip/true/crop/1063x708+0+645/resize/2600x1732!/format/webp/quality/90/?url=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2Fk7DNccS6Kr4etAxoP-1e9r_Us1M%3D%2F0x0%3A1063x1409%2F1063x1409%2Ffilters%3Afocal%28469x999%3A470x1000%29%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_asset%2Ffile%2F23327779%2F2022.jpg",
   },
   {
     id: 19,
@@ -177,7 +177,7 @@ const pets = [
     color: "Black",
     specialSkill: "Uses litter box at appropriate hours.",
     type: "cat",
-    imageUrl: "http://www.funnycatsite.com/pictures/Lazy_White_Cat.jpg",
+    imageUrl: "https://animalwellnessmagazine.com/wp-content/uploads/shutterstock_15822262-e1536681040625.jpg",
   },
   {
     id: 21,
@@ -186,7 +186,7 @@ const pets = [
     specialSkill: "Owns a Nintendo Power Glove.",
     type: "dino",
     imageUrl:
-      "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715",
+      "https://media-cdn.tripadvisor.com/media/photo-s/06/09/9b/af/museum-of-life-science.jpg",
   },
   {
     id: 22,
@@ -195,7 +195,7 @@ const pets = [
     specialSkill: "Is comfortable with jokes about his receding hairline.",
     type: "cat",
     imageUrl:
-      "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg",
+      "https://www.bluevalleyanimalhospital.net/blog/wp-content/uploads/2019/08/iStock-495910314.jpg",
   },
   {
     id: 23,
@@ -214,7 +214,7 @@ const pets = [
       "Participates in karaoke but does not force others to go out to karaoke.",
     type: "dog",
     imageUrl:
-      "http://www.dogbreedplus.com/dog_breeds/images/basset-hound-4.jpg",
+      "https://static01.nyt.com/images/2019/06/17/science/17DOGS/17DOGS-mobileMasterAt3x-v2.jpg",
   },
   {
     id: 25,
@@ -223,7 +223,7 @@ const pets = [
     specialSkill: "Knows the words to 4 rap songs.",
     type: "cat",
     imageUrl:
-      "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg",
+      "https://especiallycats.com/media/9d9-Cat14.jpg",
   },
   {
     id: 26,
@@ -268,7 +268,7 @@ const pets = [
     specialSkill: "Doesn’t get weirded out by the word “moist.”",
     type: "dino",
     imageUrl:
-      "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119",
+      "https://www.thesun.co.uk/wp-content/uploads/2019/03/fullbod2.png",
   },
 ];
 
